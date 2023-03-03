59ddd26b3e6735fbe9f6b0b0f8d5f7fb8d0c4dd2
blueberrysusanliao <blueberryliao@163.com>
2023-03-03 23:25:16 +0800

    hello

---
+++ 01-hello.py
@@ -0,0 +1,21 @@
+print('hello world')
+print(48/1.52/1.52)
+# print(1.52*1.52*18.5)
+
+    
+# result of input is string
+# user_age=input('please enter your age:')
+# print('I know you are '+user_age+' years\' old')
+
+print(type(11.3))
+print(type('this is a sentence'))
+num_str = str(1212)
+print(type(num_str), num_str)
+
+if True:
+    print("It's true")
+else:
+    print("It's false")
+
+f = open('file.txt', 'w', encoding='utf-8')
+
